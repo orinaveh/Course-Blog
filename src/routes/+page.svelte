7599b061dd9ccf44a57440383ed4422fce9a97af
Ori Naveh <orinaveh1@gmail.com>
2022-09-09 22:38:37 +0300

    wip: im way too tired

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Intro, { metadata } from '../posts/intro.svx';
+
+    const { title } = metadata;
+
+</script>
+
+<h1>{title}</h1>
+<Intro />
